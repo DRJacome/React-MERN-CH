baf585259c8cbe5dd63084ccea178594c90b2ffb
David Rodríguez <jacodaro@hotmail.com>
2023-10-19 21:30:11 +0200

    10 Profundizando hooks: useReducer

---
+++ 10-profundizando-hooks-usereducer/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { TareasApp } from './components/08-use-reducer/TareasApp';
+/* import { Padre } from './components/07-tarea-memo/Padre'; */
+/* import { CallbackHook } from './components/06-memos/CallbackHook'; */
+/* import { MemoHook } from './components/06-memos/MemoHook'; */
+/* import { Memorize } from './components/06-memos/Memorize'; */
+/* import { Layout } from './components/05-useLaoyutEffect/LayoutEffect'; */
+/* import { EjemploRealUseRef } from './components/04-useRef/EjemploRealUseRef'; */
+/* import { FocusScreen } from './components/03-useRef/FocusScreen'; */
+/* import { MultipleCustomHooks } from './components/03-ejemplos/MultipleCustomHooks'; */
+/* import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'; */
+/* import { SimpleForm } from './components/02-useEffect/SimpleForm'; */
+/* import { CounterApp } from './components/01-useState/CounterApp'; */
+/* import { HookApp } from './HookApp'; */
+/* import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'; */
+/* import './components/01-useState/counter.css'  */
+/* import './components/08-use-reducer/Intro-reducer' */
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <TareasApp />
+);
+ 
